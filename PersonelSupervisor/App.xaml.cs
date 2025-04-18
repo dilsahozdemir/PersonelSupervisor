3cf9c94d252fd718c5e3fd4cd49eed8b4d5b4e45
Dilşah ÖZDEMİR <153947474+dilsahozdemir@users.noreply.github.com>
2025-04-18 13:36:21 +0300

    klasörler

--- PersonelSupervisor/App.xaml.cs
+++ PersonelSupervisor/App.xaml.cs
@@ -9,5 +9,13 @@ namespace PersonelSupervisor;
 /// </summary>
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        // Veritabanı başlat
+        DatabaseHelper.InitializeDatabase();
+    }
 }
+
 
